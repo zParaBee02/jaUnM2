2efaeff75ae138b2f46f149c2a315e65e9bc11c4
Franz.uc <franz.uc@gmail.com>
2022-10-10 22:04:59 -0400

    Clase 1 Dinosaur

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,58 @@
+import pygame
+
+from dino_runner.utils.constants import RUNNING, JUMPING
+
+
+class Dinosaur:
+    X_POS = 80
+    Y_POS = 310
+
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.image = RUNNING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.dino_run = True
+        self.dino_jump = False
+
+    def update (self, user_input):
+        if self.dino_run:
+            self.run()
+        elif self.dino_jump:
+            self.jump()
+
+        if  user_input[pygame.K_UP] and not self.dino_jump:
+            self.dino_jump = True
+            self.dino_run = False
+        elif not self.dino_jump:
+            self.dino_jump = False
+            self.dino_run = True
+
+        if self.step_index >= 10:
+            self.step_index = 0
+
+    def jump (self):
+        self.image = JUMPING
+        self.dino_rect.y -= self.jump_vel * 4
+
+        self.jump_vel -= 0.8
+        if self.jump_vel < -8.5:
+            self.dino_rect.y += self.Y_POS
+            self.dino_jump = False
+            self.jump_vel = self.JUMP_VEL
+
+    def run(self):
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.step_index += 1
+
+    def draw (self, screen):
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+
+
